4ae8cf8fa7c01f1f67c480bb46dee5e5f54a626b
ArashkKH <106018197+ArashkKH@users.noreply.github.com>
2022-08-25 11:58:51 +0430

    virgool complete

--- Virgool/master.js
+++ Virgool/master.js
@@ -1,19 +1,60 @@
-
 //note: how to get full scroll when main is 100vh???
 let fullScroll
+
 function getScroll() {
-    fullScroll   = (document.getElementsByTagName('main')[0].clientHeight) - (document.getElementsByTagName('html')[0].clientHeight)
+    fullScroll = (document.getElementById('content').clientHeight) - (document.getElementsByTagName('html')[0].clientHeight)
+    // console.log(fullScroll)
+
 }
 getScroll()
 window.addEventListener('resize', getScroll)
-alert(fullScroll)
 
 
 
 let currentScroll
+currentScroll = document.getElementsByTagName('main')[0].scrollTop
 
 document.getElementsByTagName('main')[0].addEventListener('scroll', () => {
     currentScroll = document.getElementsByTagName('main')[0].scrollTop
-    console.log(currentScroll)
-    // alert('')
+    // console.log(currentScroll)
+
+    document.getElementsByClassName('line')[0].style.width=(((currentScroll/fullScroll)*100) +'%')
 })
+let e = document.getElementById('progBar')
+document.getElementById('progBar').addEventListener('click',(ev)=>{
+
+    document.getElementsByTagName('main')[0].classList.add('smooth')
+
+    document.getElementsByTagName('main')[0].scrollTo(
+        0,(((ev.screenX)/(e.clientWidth))*fullScroll)
+    )
+    console.log((e.clientWidth))
+
+    document.getElementsByTagName('main')[0].classList.remove('smooth')
+
+},true)
+
+function goUp() {
+    document.getElementsByTagName('main')[0].scrollTo(0, currentScroll - 20)
+}
+
+function goDown() {
+    document.getElementsByTagName('main')[0].scrollTo(0, currentScroll + 20)
+}
+
+let buttons = document.querySelectorAll('.controlbtn')
+let temp
+buttons.forEach(element => {
+    element.addEventListener('mousedown', () => {
+        element.classList.add('activeControl')
+        if (element.getAttribute('data-direction') == 'up') {
+            temp = setInterval(goUp)
+        } else if (element.getAttribute('data-direction') == 'down') {
+            temp = setInterval(goDown)
+        }
+    })
+    element.addEventListener('mouseup', () => {
+        element.classList.remove('activeControl')
+        clearInterval(temp)
+    })
+});
